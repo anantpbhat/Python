222c4eccb6efdc95af00658340b6c216f6a36e5f
Anant Bhat <anant.bhat@gmail.com>
2022-06-01 07:45:24 -0400

    Comit - Jun 1st

--- trysockets.py
+++ trysockets.py
@@ -5,3 +5,13 @@ import socket
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 9090
 Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+Server.bind()(HOST, PORT))
+Server.listen(5)
+
+while true:
+    communication_socket, address = Server.accept()
+    print("Connect to IP: %s" % address)
+    mesg = communication_socket.recv(1024).decode('utf-8')
+    print("Message from Client: %s" % mesg)
+    
