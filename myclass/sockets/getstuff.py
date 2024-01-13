ea904526a8fba3e8c1c19af5cbd1b1a4f2e4a38f
Anant Bhat <anant.bhat@gmail.com>
2024-01-12 19:26:29 -0500

    Padded date & time

--- myclass/sockets/getstuff.py
+++ myclass/sockets/getstuff.py
@@ -4,6 +4,6 @@ from datetime import datetime
 class Getstuff():
     def getdttm(self):
         dttm = datetime.now()
-        dt = (str(dttm.year) + "-" + str(dttm.month) + "-" + str(dttm.day)).encode('utf-8')
-        tm = (str(dttm.hour) + ":" + str(dttm.minute) + ":" + str(dttm.second)).encode('utf-8')
+        dt = (str(dttm.year) + "-" + str("%02d" % dttm.month) + "-" + str("%02d" % dttm.day)).encode('utf-8')
+        tm = (str("%02d" % dttm.hour) + ":" + str("%02d" % dttm.minute) + ":" + str("%02d" % dttm.second)).encode('utf-8')
         return(dt, tm)
