3fe9533f8bf7ca6b6970003a08524d56797f061a
Anant Bhat <anant.bhat@gmail.com>
2024-01-14 09:55:08 -0500

    Removed not needed encoding

--- myclass/sockets/getstuff.py
+++ myclass/sockets/getstuff.py
@@ -4,6 +4,6 @@ from datetime import datetime
 class Getstuff():
     def getdttm(self):
         dttm = datetime.now()
-        dt = (str(dttm.year) + "-" + str("%02d" % dttm.month) + "-" + str("%02d" % dttm.day)).encode('utf-8')
-        tm = (str("%02d" % dttm.hour) + ":" + str("%02d" % dttm.minute) + ":" + str("%02d" % dttm.second)).encode('utf-8')
+        dt = str(dttm.year) + "-" + str("%02d" % dttm.month) + "-" + str("%02d" % dttm.day)
+        tm = str("%02d" % dttm.hour) + ":" + str("%02d" % dttm.minute) + ":" + str("%02d" % dttm.second)
         return(dt, tm)
