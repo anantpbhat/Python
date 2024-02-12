33ff4847274c2b30e827e767ee4658a99643b075
Anant Bhat <anant.bhat@gmail.com>
2024-02-11 21:00:36 -0500

    Added route web-demo for flask

--- web-demo.py
+++ web-demo.py
@@ -1,11 +1,13 @@
+import re
 
 from flask import Flask
+import re
 
 app = Flask(__name__)
 
 @app.route('/')     ###127.0.0.1:5000/
 def index():
-    return "<h1>Hello World!</h1>"
+    return "<h1>Hello World! Try 'puppy_latin' page!!</h1>"
 
 @app.route('/info')     ###127.0.0.1:5000/info
 def info():
@@ -14,6 +16,14 @@ def info():
 @app.route('/person/<fullname>')
 def person(fullname):
     return "<h2>Page for %s</h2>" % fullname.upper()
+
+@app.route('/puppy_latin/<pupname>')
+def puppylatin(pupname):
+    if pupname.endswith('y'):
+        latinpup = pupname.rstrip('y') + 'iful'
+    else:
+        latinpup = str(pupname) + 'y'
+    return f"<h1>Latin name for {pupname} is {latinpup}</h1>"
 
 
 if __name__ == "__main__":
