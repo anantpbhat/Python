28ff6384816ff58155d49589255ac7c122d7e6cd
Anant Bhat <anant.bhat@gmail.com>
2024-02-13 14:46:53 -0500

    Render test from flask

--- web-demo.py
+++ web-demo.py
@@ -1,21 +1,15 @@
-import re
 
-from flask import Flask
-import re
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')     ###127.0.0.1:5000/
 def index():
-    return "<h1>Hello World! Try 'puppy_latin' page!!</h1>"
+    return "<h1>Hello World! Try puppy_latin page OR render a template!!</h1>"
 
 @app.route('/info')     ###127.0.0.1:5000/info
 def info():
     return "<h1>Website informational page.</h1>"
-
-@app.route('/person/<fullname>')
-def person(fullname):
-    return "<h2>Page for %s</h2>" % fullname.upper()
 
 @app.route('/puppy_latin/<pupname>')
 def puppylatin(pupname):
@@ -24,6 +18,10 @@ def puppylatin(pupname):
     else:
         latinpup = str(pupname) + 'y'
     return f"<h1>Latin name for {pupname} is {latinpup}</h1>"
+
+@app.route('/testform')
+def testform():
+    return render_template('testhtml.html')
 
 
 if __name__ == "__main__":
