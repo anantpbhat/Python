cf17e2a2236fe7e4f96dff11d73e26db398720d6
Anant Bhat <anant.bhat@gmail.com>
2024-02-17 19:57:18 -0500

    Form test in flask

--- web-demo.py
+++ web-demo.py
@@ -1,15 +1,11 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')     ###127.0.0.1:5000/
 def index():
-    return "<h1>Hello World! Try puppy_latin page OR render a template!!</h1>"
-
-@app.route('/info')     ###127.0.0.1:5000/info
-def info():
-    return "<h1>Website informational page.</h1>"
+    return render_template('index.html')
 
 @app.route('/puppy_latin/<pupname>')
 def puppylatin(pupname):
@@ -19,10 +15,15 @@ def puppylatin(pupname):
         latinpup = str(pupname) + 'y'
     return f"<h1>Latin name for {pupname} is {latinpup}</h1>"
 
-@app.route('/testform')
-def testform():
-    return render_template('testhtml.html')
+@app.route('/signup_form')
+def signup_form():
+    return render_template('signup.html')
 
+@app.route('/thank_you')     ###127.0.0.1:5000/info
+def thank_you():
+    first = request.args.get('first')
+    last = request.args.get('last')
+    return render_template('thankyou.html', first=first, last=last)
 
 if __name__ == "__main__":
     app.run()
