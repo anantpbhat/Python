86b4ba1f631091efca61f51f5c2b88ea1c4fba76
Anant Bhat <anant.bhat@gmail.com>
2024-02-18 09:21:40 -0500

    Error handler added

--- web-demo.py
+++ web-demo.py
@@ -25,5 +25,9 @@ def thank_you():
     last = request.args.get('last')
     return render_template('thankyou.html', first=first, last=last)
 
+@app.errorhandler(404)
+def 404_error(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.run()
