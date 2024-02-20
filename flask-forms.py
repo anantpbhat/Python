ddf9a3f6e09af3d62365f7282e7af6b163f80dc3
Anant Bhat <anant.bhat@gmail.com>
2024-02-19 21:30:20 -0500

    Code added for flask flash

--- flask-forms.py
+++ flask-forms.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, flash, session, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
@@ -6,17 +6,19 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "mysecretkey"
 
 class InfoForm(FlaskForm):
-    breed = StringField('What breed are you?')
-    submit = SubmitField('Submit')
+    breed = StringField('What Dog breed is your pet?')
+    submit = SubmitField('Click Here')
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
     breed = False
     form = InfoForm()
     if form.validate_on_submit():
-        breed = form.breed.data
+        session['breed'] = form.breed.data
+        flash('Dog breed you specified was - %s' % session['breed'])
         form.breed.data = ''
-    return render_template('home.html', form=form, breed=breed)
+        return redirect(url_for('home'))
+    return render_template('home.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
