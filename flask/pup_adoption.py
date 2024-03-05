7288ac50505fd8d30a6e2b55496b5342a98e8215
Anant Bhat <anant.bhat@gmail.com>
2024-03-05 14:35:44 -0500

    Latest update for puppy with toys & owner additions

--- flask/pup_adoption.py
+++ flask/pup_adoption.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, url_for, redirect
-from pup_forms import AddForm, DelForm
+from flask import Flask, render_template, url_for, redirect, flash
+from pup_forms import AddForm, DelForm, OwnerForm, ToyForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
@@ -25,11 +25,7 @@ class Puppy(db.Model):
         self.name = name
 
     def list_toys(self):
-        Toys = []
-        if self.toys:
-            for Ty in self.toys:
-                Toys.append(Ty.item_name)
-            toy_list = ','.join(Toys)
+        toy_list = ';'.join([Ty.item_name for Ty in self.toys])
         return toy_list
 
     def __repr__(self):
@@ -73,18 +69,41 @@ def add_pup():
     aform = AddForm()
     if aform.validate_on_submit():
         name = aform.name.data
-        owner = aform.owner.data
-        toys = aform.toys.data
         new_pup = Puppy(name)
         db.session.add(new_pup)
         db.session.commit()
-        pup = Puppy.query.filter_by(name=name).first()
-        pup_owner = Owner(owner, pup.id)
-        pup_toys = Toy(toys, pup.id)
-        db.session.add_all([pup_owner, pup_toys])
-        db.session.commit()
-        return redirect(url_for('list_pup'))
+        flash("Puppy %s - Successfully added!" % name)
+        return redirect(url_for('add_pup'))
     return render_template('pup_add.html', form = aform)
+
+@app.route('/owner', methods=['GET', 'POST'])
+def owner_pup():
+    oform = OwnerForm()
+    if oform.validate_on_submit():
+        owner = oform.owner.data
+        id = oform.id.data
+        pup_owner = Owner(owner, id)
+        db.session.add(pup_owner)
+        db.session.commit()
+        pup_name = pup_owner.puppy.name
+        flash("Owner %s for Puppy %s - Successfully added!" % (pup_owner.name, pup_name))
+        return redirect(url_for('owner_pup'))
+    return render_template('pup_owner.html', form = oform)
+
+@app.route('/toys', methods=['GET', 'POST'])
+def toys_pup():
+    tform = ToyForm()
+    if tform.validate_on_submit():
+        toys = tform.toys.data
+        id = tform.id.data
+        pup_toys = Toy(toys, id)
+        db.session.add(pup_toys)
+        db.session.commit()
+        pup_name = pup_toys.puppy.name
+        puppy = Puppy(pup_name)
+        flash("Toys %s for Puppy %s - Successfully added!" % (puppy.list_toys(), pup_name))
+        return redirect(url_for('toys_pup'))
+    return render_template('pup_toys.html', form = tform)
 
 @app.route('/list')
 def list_pup():
@@ -99,7 +118,8 @@ def del_pup():
         rm_pup = Puppy.query.get(id)
         db.session.delete(rm_pup)
         db.session.commit()
-        return redirect(url_for('list_pup'))
+        flash("Puppy with ID %s - Successfully deleted!!" % id)
+        return redirect(url_for('del_pup'))
     return render_template('pup_del.html', form = dform)
 
 
--- flask/pup_forms.py
+++ flask/pup_forms.py
@@ -3,10 +3,19 @@ from wtforms import StringField, IntegerField, SubmitField
 
 class AddForm(FlaskForm):
     name = StringField('Name of the Puppy: ')
-    owner = StringField('Owner Name: ')
     toys = StringField("Puppy's Toy items: ")
     submit = SubmitField('Add Puppy')
 
 class DelForm(FlaskForm):
     id = IntegerField("ID number of the Puppy to be removed: ")
     submit = SubmitField('Delete Puppy')
+
+class OwnerForm(FlaskForm):
+    owner = StringField('Owner Name: ')
+    id = IntegerField("ID number of the Puppy: ")
+    submit = SubmitField('Add Owner')
+
+class ToyForm(FlaskForm):
+    toys = StringField("Puppy's Toy item/s: ")
+    id = IntegerField("ID number of the Puppy: ")
+    submit = SubmitField('Add Toy/s')
