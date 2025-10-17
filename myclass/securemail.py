0f0f3e99c06fae326bc8aed64e793f79bbe66129
anantpbhat <42523261+anantpbhat@users.noreply.github.com>
2025-10-17 09:11:50 -0400

    Indents fixed

--- myclass/securemail.py
+++ myclass/securemail.py
@@ -33,10 +33,10 @@ class SendEmail():
         encoders.encode_base64(apart)
         apart.add_header("Content-Disposition", f"attachment; filename={atchfl}",)
         message.attach(apart)
-        context = ssl.create_default_context()
-        with smtplib.SMTP(smtp_server, port) as emsrvr:
-          emsrvr.starttls(context=context)
-          emsrvr.login(smtp_login, em_pswd)
-          emsrvr.sendmail(sndr_email, rcvr_eml_lst, message.as_string())
-          print("Email Sent...")
-        return
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as emsrvr:
+      emsrvr.starttls(context=context)
+      emsrvr.login(smtp_login, em_pswd)
+      emsrvr.sendmail(sndr_email, rcvr_eml_lst, message.as_string())
+      print("Email Sent...")
+    return
